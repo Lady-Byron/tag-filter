a59496f5b00c040bbfa726580351a22eb35eb6d9
Lady-Byron <samantha-everett@outlook.com>
2025-09-11 02:45:06 -0400

    Update TagChip.tsx

--- js/src/forum/components/TagChip.tsx
+++ js/src/forum/components/TagChip.tsx
@@ -2,7 +2,7 @@ import tagIcon from 'flarum/tags/common/helpers/tagIcon';
 import type Tag from 'flarum/tags/common/models/Tag';
 
 /**
- * 彩色+图标的“紧密芯片”，复刻 tags-filter 的展示思路：
+ * 彩色+图标的“紧密芯片”
  * - 用 CSS 变量 --tag-title-color 驱动文字着色（background-clip:text）
  * - 选中且无自定义图标时显示对勾
  */
@@ -19,16 +19,16 @@ export default function TagChip(
       style={{ '--tag-title-color': color } as any}
       onclick={opts.onclick}
     >
-      <span class="Button-label">
-        <i class="SelectTagListItem-icon">
+      <span className="Button-label">
+        <i className="SelectTagListItem-icon">
           {tag.icon()
             ? tagIcon(tag, { className: 'SelectTagListItem-tagIcon' })
             : selected
-              ? <i class="icon TagIcon fas fa-check SelectTagListItem-checkIcon" />
+              ? <i className="icon TagIcon fas fa-check SelectTagListItem-checkIcon" />
               : tagIcon(tag, { className: 'SelectTagListItem-tagIcon' })}
         </i>
-        <span class="SelectTagListItem-name">{tag.name()}</span>
-        {tag.description() ? <span class="SelectTagListItem-description">{tag.description()}</span> : null}
+        <span className="SelectTagListItem-name">{tag.name()}</span>
+        {tag.description() ? <span className="SelectTagListItem-description">{tag.description()}</span> : null}
       </span>
     </button>
   );
